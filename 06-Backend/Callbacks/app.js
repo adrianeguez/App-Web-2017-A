08b8b4cd6e9b212cdb1da0cec05813ded1ee68ba
USRDEL <USRDEL@USRDEL-Profesor.local>
2017-06-05 20:41:39 -0500

    Muchas cosas

--- 06-Backend/Callbacks/app.js
+++ 06-Backend/Callbacks/app.js
@@ -4,7 +4,44 @@
 
 const fs = require('fs');
 
+// npm install twj-a-eguez
+
+const calculadora = require('twj-a-eguez');
+
+const Passwords = require('machinepack-passwords');
+
+let a = 2,
+    b = 3,
+    resultado;
+
+resultado = calculadora.calculadoraUdla.sumar(a, b);
+
+console.log(`Resultado es: ${resultado}`);
+
+let passswordAEncriptar = "1234";
+
+Passwords.encryptPassword(
+    {
+        password: passswordAEncriptar,
+    })
+    .exec({
+        error: (error)=> {
+            console.log(`Error: ${error}`);
+        },
+        success: (res)=> {
+            console.log(`Resultado: ${res}`);
+
+        },
+    });
+
+
 console.log("Empieza");
+
+// 1 - Nombre y ruta del archivo
+// 2 - Codificacion
+// 3 - Anonymous Function
+// (error!,contenido del archivo )
+
 
 fs.readFile('texto.txt', 'utf8',
     (err, data) => {
---
+++ 06-Backend/NPM/README.md
@@ -0,0 +1,26 @@
+****
+
+# Bienvenidos
+## Calculadora Udla
+
+Este paquete nos ayuda a sumar, restar y multiplicar.
+
+```javascript
+calculadoraUdla = {
+    sumar:(n1,n2)=>{
+        return n1+n2;
+    },
+    restar:(n1,n2)=>{
+        return n1-n2;
+    },
+    multiplicar:(n1,n2)=>{
+        return n1*n2;
+    }
+};
+```
+
+## Comando para crear un package.json
+
+```
+> npm init
+```

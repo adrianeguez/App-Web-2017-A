bfc8aec96a78781c55323dd2e4d3f157f76b3b00
USRDEL <USRDEL@USRDEL-Profesor.local>
2017-06-05 20:56:22 -0500

    Comentar Actualizar paquetes

--- 06-Backend/Callbacks/app.js
+++ 06-Backend/Callbacks/app.js
@@ -4,43 +4,13 @@
 
 const fs = require('fs');
 
-// npm install twj-a-eguez
-
-const calculadora = require('twj-a-eguez');
-
-const Passwords = require('machinepack-passwords');
-
-let a = 2,
-    b = 3,
-    resultado;
-
-resultado = calculadora.calculadoraUdla.sumar(a, b);
-
-console.log(`Resultado es: ${resultado}`);
-
-let passswordAEncriptar = "1234";
-
-Passwords.encryptPassword(
-    {
-        password: passswordAEncriptar,
-    })
-    .exec({
-        error: (error)=> {
-            console.log(`Error: ${error}`);
-        },
-        success: (res)=> {
-            console.log(`Resultado: ${res}`);
-
-        },
-    });
-
-
 console.log("Empieza");
 
-// 1 - Nombre y ruta del archivo
-// 2 - Codificacion
-// 3 - Anonymous Function
-// (error!,contenido del archivo )
+// CALLBACK
+//
+//
+//
+//
 
 
 fs.readFile('texto.txt', 'utf8',
@@ -50,3 +20,45 @@ fs.readFile('texto.txt', 'utf8',
     });
 
 console.log("Termina");
+
+
+
+
+// npm install twj-a-eguez
+
+/*
+ const calculadora = require('twj-a-eguez');
+
+ let a = 2,
+ b = 3,
+ resultado;
+
+ resultado = calculadora.calculadoraUdla.sumar(a, b);
+
+ console.log(`Resultado es: ${resultado}`);
+
+ let passswordAEncriptar = "1234";
+
+ calculadora.calculadoraUdla.passwords.encryptPassword(
+ {
+ password: passswordAEncriptar,
+ })
+ .exec({
+ error: (error)=> {
+ console.log(`Error: ${error}`);
+ },
+ success: (res)=> {
+ console.log(`Resultado: ${res}`);
+
+ },
+ });
+
+
+
+
+ // 1 - Nombre y ruta del archivo
+ // 2 - Codificacion
+ // 3 - Anonymous Function
+ // (error!,contenido del archivo )
+
+ */

a52bf78e4592a0a2553ee67df900f2845a9c7c40
Darwin <darwin.morocho@epn.edu.ec>
2017-07-03 20:51:44 -0500

    Autorización header fin

--- 07-Backend-MVC/CarritoCompras/api/policies/validarCabeceraAuthorization.js
+++ 07-Backend-MVC/CarritoCompras/api/policies/validarCabeceraAuthorization.js
@@ -1,9 +1,9 @@
 
 module.exports = function(req, res, next) {
 
-  console.log(res.header("Authorization"));
+  console.log(req.headers);
 
-  if(res.header("Authorization")){
+  if(req.headers.authorization){
     return next() //Tienes permiso
   }else{
     return res.forbidden("No tiene cabecera de Autorizacion") //Redirigirle al error 403!

c4924534a98c24e1a44a847d9385836a8efc62da
Darwin <darwin.morocho@epn.edu.ec>
2017-07-29 07:58:49 -0500

    atributos universidad

--- 07-Backend-MVC/CarritoCompras/api/models/Universidad.js
+++ 07-Backend-MVC/CarritoCompras/api/models/Universidad.js
@@ -8,7 +8,23 @@
 module.exports = {
 
   attributes: {
-
+    nombreCasual:{
+      type:'string',
+      required:true
+    },
+    nombreInstitucional:{
+      type:'string',
+      unique:true,
+      required:true
+    },
+    direccion:{
+      type:'string'
+    },
+    estado:{
+      type:'string',
+      enum:['Abierto','Cerrado','Clausurado'],
+      defaultsTo:'Cerrado'
+    }
   }
 };
 
